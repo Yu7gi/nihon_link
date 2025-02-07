85229773cd9bcee09465341ab9eeda6c3dc93448
Yu7gi <piano.yuna26@gmail.com>
2025-02-07 16:06:12 +0000

    ジャンル機能作成、新規投稿画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,25 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  #Top,Aboutルーティング設定
   root :to =>"homes#top"
   get "/about" => "homes#about", as: "about"
 
-  resources :users, only: [:show, :edit, :update, :destroy] do
-    collection do
-      get 'mypage'
-    end
+  #管理者側ルーティング設定
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
   end
-  resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy]
+
+  #会員側ルーティング設定
+  scope module: :public do
+    resources :users, only: [:show, :edit, :update, :destroy] do
+      collection do
+        get 'mypage'
+      end
+    end
+    resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
