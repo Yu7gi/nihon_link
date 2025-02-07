01b7d404e15eabf9639d8941cadd2938b88603b4
Yu7gi <piano.yuna26@gmail.com>
2025-02-07 19:27:01 +0000

    投稿編集機能作成、投稿関連view調整

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -20,6 +20,13 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   private
